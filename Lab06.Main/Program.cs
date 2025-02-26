db405d6ba7ad4e392414f7824fcbce068a0d949d
Christopher Hercules <chriherc215@gmail.com>
2025-02-26 11:15:08 -0700

    Finnished lab06 chris hercules

--- Lab06.Main/Program.cs
+++ Lab06.Main/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Lab06;
+﻿using System; 
+using Lab06; //  contains the implementation of the DoublyLinkedList class.
 
 namespace Lab06.Main
 {
@@ -7,33 +7,38 @@ namespace Lab06.Main
     {
         static void Main(string[] args)
         {
+            // Creating an instance of DoublyLinkedList to store integers.
             DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+            // Prompting the user to enter numbers to add to the list.
             Console.WriteLine("Enter numbers to add to the list. Press Escape to end.");
 
-            while (true)
+            while (true) //loop to continuously take user input.
             {
-                var key = Console.ReadKey(intercept: true);
-                if (key.Key == ConsoleKey.Escape)
+                var key = Console.ReadKey(intercept: true); // reading a key press without displaying it in the console.
+
+                if (key.Key == ConsoleKey.Escape) // exit
                 {
                     break;
                 }
 
-                if (char.IsDigit(key.KeyChar))
+                if (char.IsDigit(key.KeyChar)) // checking if the pressed key is a numeric digit.
                 {
-                    int value = int.Parse(key.KeyChar.ToString());
-                    list.AddLast(value);
-                    Console.WriteLine($"\nAdded {value} to the list.");
+                    int value = int.Parse(key.KeyChar.ToString()); // Converting the character input into an integer.
+                    list.AddLast(value); // adding the integer to the end of the doubly linked list.
+                    Console.WriteLine($"\nAdded {value} to the list."); // Informing .
                 }
                 else
                 {
-                    Console.WriteLine("\nPlease enter a valid digit.");
+                    Console.WriteLine("\nPlease enter a valid digit."); // Informing the user that only digits are allowed.
                 }
             }
 
+            // Displaying the final list of numbers added by the user.
             Console.WriteLine("\nFinal list:");
-            foreach (int i in list)
+            foreach (int i in list) // iterating through the doubly linked list.
             {
-                Console.WriteLine(i);
+                Console.WriteLine(i); // print
             }
         }
     }
--- Lab06/Class1.cs
+++ Lab06/Class1.cs
@@ -4,80 +4,103 @@ using System.Collections.Generic;
 
 namespace Lab06
 {
+    
+    // Represents a node in a doubly linked list.
+    
     public class DoublyLinkedListNode<T>
     {
-        public T Value { get; set; }
-        public DoublyLinkedListNode<T> Next { get; set; }
-        public DoublyLinkedListNode<T> Previous { get; set; }
+        public T Value { get; set; } // The data stored in the node
+        public DoublyLinkedListNode<T> Next { get; set; } // Pointer to the next node
+        public DoublyLinkedListNode<T> Previous { get; set; } // Pointer to the previous node
 
+        
+        // Initializes a new node with a given value.
+        
         public DoublyLinkedListNode(T value)
         {
             Value = value;
         }
     }
 
+    
+    // Defines the interface for a double-ended collection (doubly linked list).
+    
     public interface IDoubleEndedCollection<T>
     {
-        T First { get; }
-        T Last { get; }
-        int Length { get; }
-        void AddLast(T value);
-        void AddFirst(T value);
-        void RemoveFirst();
-        void RemoveLast();
-        void InsertAfter(DoublyLinkedListNode<T> node, T value);
-        void RemoveByValue(T value);
-        void ReverseList();
+        T First { get; } // Gets the first element
+        T Last { get; } // Gets the last element
+        int Length { get; } // Gets the number of elements in the list
+
+        void AddLast(T value); // Adds an element to the end
+        void AddFirst(T value); // Adds an element to the front
+        void RemoveFirst(); // Removes the first element
+        void RemoveLast(); // Removes the last element
+        void InsertAfter(DoublyLinkedListNode<T> node, T value); // Inserts an element after a given node
+        void RemoveByValue(T value); // Removes a node by value
+        void ReverseList(); // Reverses the linked list
     }
 
+    
+    // Implements a doubly linked list.
+    
     public class DoublyLinkedList<T> : IDoubleEndedCollection<T>, IEnumerable<T>
     {
-        private DoublyLinkedListNode<T> _head;
-        private DoublyLinkedListNode<T> _tail;
-        private int _count;
+        private DoublyLinkedListNode<T> _head; // First node in the list
+        private DoublyLinkedListNode<T> _tail; // Last node in the list
+        private int _count; // Number of nodes in the list
 
-        public int Length => _count;
+        public int Length => _count; // Returns the total number of elements in the list
         public T First => _head != null ? _head.Value : throw new InvalidOperationException("The list is empty.");
         public T Last => _tail != null ? _tail.Value : throw new InvalidOperationException("The list is empty.");
 
+        
+        /// Adds a new node at the beginning of the list.
+        
         public void AddFirst(T value)
         {
             var newNode = new DoublyLinkedListNode<T>(value);
             if (_head == null)
             {
-                _head = _tail = newNode;
+                _head = _tail = newNode; // If list is empty, head and tail are the same
             }
             else
             {
                 newNode.Next = _head;
                 _head.Previous = newNode;
-                _head = newNode;
+                _head = newNode; // Update head
             }
             _count++;
         }
 
+        
+        /// Adds a new node at the end of the list.
+        
         public void AddLast(T value)
         {
             var newNode = new DoublyLinkedListNode<T>(value);
             if (_tail == null)
             {
-                _head = _tail = newNode;
+                _head = _tail = newNode; // If list is empty, head and tail are the same
             }
             else
             {
                 newNode.Previous = _tail;
                 _tail.Next = newNode;
-                _tail = newNode;
+                _tail = newNode; // Update tail
             }
             _count++;
         }
 
+        
+        // Removes the first node from the list.
+        
         public void RemoveFirst()
         {
-            if (_head == null) return;
+            if (_head == null) return; // Do nothing if list is empty
+
             if (_head == _tail)
             {
-                _head = _tail = null;
+                _head = _tail = null; // If only one element, reset list
             }
             else
             {
@@ -87,12 +110,16 @@ namespace Lab06
             _count--;
         }
 
+        
+        // Removes the last node from the list.
+        
         public void RemoveLast()
         {
-            if (_tail == null) return;
+            if (_tail == null) return; // Do nothing if list is empty
+
             if (_head == _tail)
             {
-                _head = _tail = null;
+                _head = _tail = null; // If only one element, reset list
             }
             else
             {
@@ -102,24 +129,33 @@ namespace Lab06
             _count--;
         }
 
+        
+        // Inserts a new node after a given node in the list.
+        
         public void InsertAfter(DoublyLinkedListNode<T> node, T value)
         {
             if (node == null) return;
+
             var newNode = new DoublyLinkedListNode<T>(value);
             newNode.Next = node.Next;
             newNode.Previous = node;
+
             if (node.Next != null)
             {
                 node.Next.Previous = newNode;
             }
             node.Next = newNode;
+
             if (node == _tail)
             {
-                _tail = newNode;
+                _tail = newNode; // If inserting after the last node, update tail
             }
             _count++;
         }
 
+        
+        // removes the first node that matches the given value.
+        
         public void RemoveByValue(T value)
         {
             var current = _head;
@@ -141,12 +177,15 @@ namespace Lab06
                         current.Next.Previous = current.Previous;
                         _count--;
                     }
-                    return;
+                    return; // Exit after removing the first match
                 }
                 current = current.Next;
             }
         }
 
+        
+        // Reverses the order of nodes in the list.
+        
         public void ReverseList()
         {
             var current = _head;
@@ -154,12 +193,14 @@ namespace Lab06
 
             while (current != null)
             {
+                // Swap previous and next pointers
                 temp = current.Previous;
                 current.Previous = current.Next;
                 current.Next = temp;
-                current = current.Previous;
+                current = current.Previous; // Move to next node (which is now previous)
             }
 
+            // Swap head and tail
             if (temp != null)
             {
                 _tail = _head;
@@ -167,6 +208,9 @@ namespace Lab06
             }
         }
 
+        
+        // returns an enumerator that iterates through the list.
+        
         public IEnumerator<T> GetEnumerator()
         {
             var current = _head;
